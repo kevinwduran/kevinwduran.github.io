73ca1783cfb5311b971c85461c4a7bd7a929433c
kevinwduran <kevinwduran23@gmail.com>
2024-04-16 00:54:17 -0300

    changes

---
+++ README.md
@@ -0,0 +1,16 @@
+# Currículo Web - Projeto Faculdade - Engenharia de Software
+Este é um projeto de currículo web desenvolvido como parte do curso de Engenharia de Software. Ele utiliza HTML, CSS e JavaScript para criar uma página de currículo interativa e responsiva.
+
+## Recursos
+- Apresenta informações pessoais, qualificações e experiência profissional.
+- Layout responsivo que se adapta a diferentes tamanhos de tela.
+- Navegação suave usando âncoras internas.
+
+## Contribuindo
+Contribuições são bem-vindas! Se você encontrar um bug ou tiver sugestões de melhorias, sinta-se à vontade para abrir uma issue ou enviar um pull request.
+
+## Autor
+Este currículo web foi desenvolvido por [Kevin Duran](https://github.com/kevinwduran).
+
+## Link do Projeto
+O currículo web está disponível online no GitHub Pages: [Currículo Web](https://kevinwduran.github.io/)
---
+++ script.js
@@ -0,0 +1,50 @@
+// Função para adicionar efeito branco transparente no header
+const headerBg = document.querySelector(".header-bg");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= headerBg.offsetTop && window.scrollY > 0) {
+    headerBg.style.background =
+      "linear-gradient(to bottom, #fff 20%, rgba(255, 255, 255, 0) 100%)";
+  } else {
+    headerBg.style.background = "none";
+  }
+});
+
+// Função para menu hamburguer (mobile e tablet) (Responsividade)
+const close = document.querySelector(".close-icon");
+const open = document.querySelector(".menu-icon");
+const nav_ul = document.querySelector(".header-menu");
+
+function openMenu() {
+  nav_ul.classList.add("open");
+}
+function closeMenu() {
+  nav_ul.classList.remove("open");
+}
+open.addEventListener("click", openMenu);
+close.addEventListener("click", closeMenu);
+
+// Função para verificar se o elemento está na janela de visualização
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Função para adicionar a classe de animação quando o elemento estiver visível
+function checarElementosTelaAtual() {
+  const elementos = document.querySelectorAll(".animation");
+  elementos.forEach((elemento) => {
+    if (isElementInViewport(elemento)) {
+      elemento.classList.add("animate-show-right");
+    }
+  });
+}
+
+// Verificar quando a página é rolada e verificar elementos visíveis
+window.addEventListener("scroll", checarElementosTelaAtual);
